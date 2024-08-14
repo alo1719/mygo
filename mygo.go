e27c54a91700983d584764d864a949661dcdd6b9
Xinyi Zhang <zhangxinyi1719@gmail.com>
2024-08-14 15:43:15 -0700

    feat: add InSlice & InMap

--- mygo.go
+++ mygo.go
@@ -1,4 +1,3 @@
-// Package mygo v0.10.0
 package mygo
 
 // P is alias for Ptr
@@ -11,6 +10,11 @@ func V[T any](p *T) T {
 	return Value(p)
 }
 
+// In is alias for InSlice
+func In[T comparable](e T, s []T) bool {
+	return InSlice(e, s)
+}
+
 func Ptr[T any](v T) *T {
 	return &v
 }
@@ -20,4 +24,18 @@ func Value[T any](p *T) T {
 		return *new(T)
 	}
 	return *p
+}
+
+func InSlice[T comparable](e T, s []T) bool {
+	for _, v := range s {
+		if v == e {
+			return true
+		}
+	}
+	return false
+}
+
+func InMap[K comparable, V any](k K, m map[K]V) bool {
+	_, ok := m[k]
+	return ok
 }
--- mygo_test.go
+++ mygo_test.go
@@ -37,3 +37,23 @@ func TestValueStruct(t *testing.T) {
 		t.Errorf("Value(%v) = %v, want %v", p, V(p), S{})
 	}
 }
+
+func TestInSlice(t *testing.T) {
+	s := []int{1, 2, 3}
+	if !InSlice(1, s) {
+		t.Errorf("In(1, %v) = false, want true", s)
+	}
+	if InSlice(4, s) {
+		t.Errorf("In(4, %v) = true, want false", s)
+	}
+}
+
+func TestInMap(t *testing.T) {
+	m := map[int]string{1: "a", 2: "b", 3: "c"}
+	if !InMap(1, m) {
+		t.Errorf("In(1, %v) = false, want true", m)
+	}
+	if InMap(4, m) {
+		t.Errorf("In(4, %v) = true, want false", m)
+	}
+}
