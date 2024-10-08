bdaf76f74f446d70eaa702462eff3aa27478e7e4
Xinyi Zhang <zhangxinyi1719@gmail.com>
2024-10-08 13:13:14 -0700

    feat: Ordered Map & Ordered Set

--- mygo.go
+++ mygo.go
@@ -1,5 +1,20 @@
 package mygo
 
+// An Ordered Map like Python's dict
+// Not thread safe (neither is Go's built-in map)
+type Map[K comparable, V any] struct {
+	l []K
+	m map[K]V
+}
+
+// An Ordered Set
+// Not thread safe
+// Contains/Add is O(1) but Delete is O(n)
+type Set[T comparable] struct {
+	l []T
+	m map[T]struct{}
+}
+
 // P is alias for Ptr
 func P[T any](v T) *T {
 	return Ptr(v)
@@ -38,4 +53,77 @@ func InSlice[T comparable](e T, s []T) bool {
 func InMap[K comparable, V any](k K, m map[K]V) bool {
 	_, ok := m[k]
 	return ok
+}
+
+func (m *Map[K, V]) Put(k K, v V) {
+	if m.m == nil {
+		m.m = make(map[K]V)
+	}
+	if InMap(k, m.m) {
+		m.m[k] = v
+	} else {
+		m.l = append(m.l, k)
+		m.m[k] = v
+	}
+}
+
+func (m *Map[K, V]) Get(k K) (V, bool) {
+	v, ok := m.m[k]
+	return v, ok
+}
+
+func (m *Map[K, V]) Keys() []K {
+	return m.l
+}
+
+func (m *Map[K, V]) Delete(k K) {
+	delete(m.m, k)
+	for i, e := range m.l {
+		if e == k {
+			m.l = append(m.l[:i], m.l[i+1:]...)
+			break
+		}
+	}
+}
+
+func (m *Map[K, V]) Contains(k K) bool {
+	return InMap(k, m.m)
+}
+
+func (m *Map[K, V]) Len() int {
+	return len(m.l)
+}
+
+func (s *Set[T]) Add(e T) {
+	if s.m == nil {
+		s.m = make(map[T]struct{})
+	}
+	if InMap(e, s.m) {
+		return
+	} else {
+		s.l = append(s.l, e)
+		s.m[e] = struct{}{}
+	}
+}
+
+func (s *Set[T]) Contains(e T) bool {
+	return InMap(e, s.m)
+}
+
+func (s *Set[T]) Delete(e T) {
+	delete(s.m, e)
+	for i, v := range s.l {
+		if v == e {
+			s.l = append(s.l[:i], s.l[i+1:]...)
+			break
+		}
+	}
+}
+
+func (s *Set[T]) Len() int {
+	return len(s.l)
+}
+
+func (s *Set[T]) Elements() []T {
+	return s.l
 }
--- mygo_test.go
+++ mygo_test.go
@@ -57,3 +57,52 @@ func TestInMap(t *testing.T) {
 		t.Errorf("In(4, %v) = true, want false", m)
 	}
 }
+
+func TestMap(t *testing.T) {
+	m := &Map[int, string]{}
+	m.Put(1, "a")
+	m.Put(2, "b")
+	if m.Contains(3) {
+		t.Errorf("Contains(3) = true, want false")
+	}
+	if !m.Contains(1) {
+		t.Errorf("Contains(1) = false, want true")
+	}
+	m.Delete(1)
+	if m.Contains(1) {
+		t.Errorf("Contains(1) = true, want false")
+	}
+	if v, _ := m.Get(2); v != "b" {
+		t.Errorf("Get(2) = %v, want 'b'", v)
+	}
+	m.Put(2, "bb")
+	if v, _ := m.Get(2); v != "bb" {
+		t.Errorf("Get(2) = %v, want 'bb'", v)
+	}
+	m.Put(3, "c")
+	if m.Len() != 2 {
+		t.Errorf("Len() = %v, want 2", m.Len())
+	}
+	t.Log(m.Keys())
+}
+
+func TestSet(t *testing.T) {
+	s := &Set[int]{}
+	s.Add(1)
+	s.Add(2)
+	if s.Contains(3) {
+		t.Errorf("Contains(3) = true, want false")
+	}
+	if !s.Contains(1) {
+		t.Errorf("Contains(1) = false, want true")
+	}
+	s.Delete(1)
+	if s.Contains(1) {
+		t.Errorf("Contains(1) = true, want false")
+	}
+	s.Add(3)
+	if s.Len() != 2 {
+		t.Errorf("Len() = %v, want 1", s.Len())
+	}
+	t.Log(s.Elements())
+}
